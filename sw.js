36acfcce4a94d1ee94d596e06be4839e23b824a9
Teasy <100031783+Teasyzi@users.noreply.github.com>
2026-09-03 22:42:13 -0300

    V36.12 RC2 hotfix: bump cache apos correção do boot PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Elo PWA + Firebase Cloud Messaging background notifications */
-const CACHE = 'elo-v36-12-rc2-apk-primary-20260903-2202';
+const CACHE = 'elo-v36-12-rc2-boot-hotfix-20260903-2230';
 const CORE=[
   './',
   './index.html',
